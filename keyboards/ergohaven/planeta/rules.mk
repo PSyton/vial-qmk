e06e63288e335ac3050583aa29ebfeeaad7ddff2
kissetfall <j@ergohaven.xyz>
2023-07-20 12:41:07 +0300

    2.9

--- keyboards/ergohaven/planeta/rules.mk
+++ keyboards/ergohaven/planeta/rules.mk
@@ -1,10 +1,21 @@
+# MCU name
 MCU = RP2040
 BOARD = GENERIC_RP_RP2040
 BOOTLOADER = rp2040
-# ALLOW_WARNINGS = yes
-# PICO_INTRINSICS_ENABLED = no
 
-DEFAULT_FOLDER = ergohaven/planeta/rev2
+# Bootloader selection
+ALLOW_WARNINGS = yes
+PICO_INTRINSICS_ENABLED = no
 
-AUDIO_ENABLE = yes
+
+# Build Options
+AUDIO_ENABLE = yes           # Audio output
 AUDIO_DRIVER = pwm_hardware
+LTO_ENABLE = no
+VIA_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+COMBO_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
+DYNAMIC_MACRO_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+REPEAT_KEY_ENABLE = yes
