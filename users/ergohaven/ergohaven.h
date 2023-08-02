c28dcec9d491c6787cb638103e04b6aaead22fe5
Evgeny <j@ergohaven.xyz>
2023-08-02 23:33:51 +0300

    userspace initial implementation

--- users/ergohaven/ergohaven.h
+++ users/ergohaven/ergohaven.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include "quantum.h"
 
 #define _BASE 0
@@ -24,39 +25,11 @@
 #define ADJUST    MO(_ADJUST)
 #define PREVWRD   LCTL(KC_LEFT)
 #define NEXTWRD   LCTL(KC_RIGHT)
-#define CTRLUP    LCTL(KC_UP)
-#define CTRLDN    LCTL(KC_DOWN)
-#define LANG      LALT(KC_LSFT)
+#define CT_Q  LCTL_T(KC_Q)
+#define CT_CM RCTL_T(KC_COMM)
+#define SF_Z  LSFT_T(KC_Z)
+#define SF_SS RSFT_T(KC_SLSH)
+#define AL_X  LALT_T(KC_X)
+#define AL_DT RALT_T(KC_DOT)
+#define LO_TB LT(LOWER, KC_TAB)
 
-#define CT_Q      LCTL_T(KC_Q)
-#define CT_CM     RCTL_T(KC_COMM)
-#define SF_Z      LSFT_T(KC_Z)
-#define SF_SS     RSFT_T(KC_SLSH)
-#define AL_X      LALT_T(KC_X)
-#define AL_DT     RALT_T(KC_DOT)
-#define LO_TB     LT(LOWER, KC_TAB)
-
-#define APP_A     LSG(KC_A)
-#define APP_S     LSG(KC_S)
-#define APP_D     LSG(KC_D)
-#define APP_F     LSG(KC_F)
-#define APP_G     LSG(KC_G)
-#define APP_X     LSG(KC_X)
-#define APP_C     LSG(KC_C)
-#define APP_V     LSG(KC_V)
-#define APP_B     LSG(KC_B)
-
-enum custom_keycodes {
-    NEXTSEN = QK_KB,
-    PREDL, 
-    BRACES,
-    PARENTH,
-    ALT_TAB,
-    LAYER_NEXT,
-    LAYER_PREV,
-};
-
-// 1st layer on the cycle
-#define LAYER_CYCLE_START 0
-// Last layer on the cycle
-#define LAYER_CYCLE_END   15
