18a44cdeaf97bc4e49732eccec4c2a799cc870e1
Сысолятин Павел Владимирович <p.sisolyatin@2gis.ru>
2024-11-09 16:00:23 +0700

    Keymap update. Nav and symbol layers.
    * Fix os specific keys.
    * '/' and '\' on same key.
    * Move 'ё' and 'ъ' to symbols.

--- quantum/qmk_settings.c
+++ quantum/qmk_settings.c
@@ -260,9 +260,9 @@ uint16_t qs_get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     return QS.tapping_term;
 }
 
-//bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
-//    return QS.tapping & 1;
-//}
+bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
+    return QS.tapping & 1;
+}
 
 bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
     return false;
