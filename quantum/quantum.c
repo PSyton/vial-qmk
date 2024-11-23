408a728f652f1a86eabcce852270ca25afcc0159
Joel Challis <git@zvecr.com>
2024-11-23 10:09:34 +0000

    Add keycode PDF(layer) to set the default layer in EEPROM (#24630)
    * [Feature] Add keycode PDF(layer) to set the default layer in EEPROM (#21881)
    
    * Apply suggestions from code review
    
    Co-authored-by: Nick Brassel <nick@tzarc.org>
    
    ---------
    
    Co-authored-by: Nebuleon <2391500+Nebuleon@users.noreply.github.com>
    Co-authored-by: Nick Brassel <nick@tzarc.org>

--- quantum/quantum.c
+++ quantum/quantum.c
@@ -53,6 +53,10 @@
 #    include "process_midi.h"
 #endif
 
+#if !defined(NO_ACTION_LAYER)
+#    include "process_default_layer.h"
+#endif
+
 #ifdef PROGRAMMABLE_BUTTON_ENABLE
 #    include "process_programmable_button.h"
 #endif
@@ -413,6 +417,9 @@ bool process_record_quantum_helper(uint16_t keycode, keyrecord_t *record) {
 #endif
 #ifdef TRI_LAYER_ENABLE
             process_tri_layer(keycode, record) &&
+#endif
+#if !defined(NO_ACTION_LAYER)
+            process_default_layer(keycode, record) &&
 #endif
 #ifdef LAYER_LOCK_ENABLE
             process_layer_lock(keycode, record) &&
